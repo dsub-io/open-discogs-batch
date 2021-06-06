5c9084b337dd2d33888ae95d68921e433c06fb42
state303 <shbae0121@gmail.com>
2021-06-06 16:53:24 +0900

    modifies supplier and query builders

--- common/src/main/java/io/dsub/discogsdata/common/exception/FileFetchException.java
+++ common/src/main/java/io/dsub/discogsdata/common/exception/FileFetchException.java
@@ -1,11 +1,11 @@
-package io.dsub.discogsdata.batch.exception;
+package io.dsub.discogsdata.common.exception;
 
 import io.dsub.discogsdata.common.exception.BaseException;
 import org.springframework.http.HttpStatus;
 
-public class FileClearException extends BaseException {
+public class FileFetchException extends BaseException {
 
-  public FileClearException(String reason) {
+  public FileFetchException(String reason) {
     super(HttpStatus.INTERNAL_SERVER_ERROR, reason);
   }
 }
