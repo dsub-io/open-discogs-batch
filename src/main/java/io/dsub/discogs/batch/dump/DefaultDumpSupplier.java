1e053ad9365feb78edcdb6a52967790f5f932981
state303 <state303@dsub.io>
2026-07-31 18:00:41 +0900

    fix: encode dump manifest download path

--- src/main/java/io/dsub/discogs/batch/dump/DefaultDumpSupplier.java
+++ src/main/java/io/dsub/discogs/batch/dump/DefaultDumpSupplier.java
@@ -90,8 +90,6 @@ public class DefaultDumpSupplier implements DumpSupplier {
   private static final List<String> KNOWN_NODE_TYPES = List.of(KEY, ETAG, SIZE);
 
   private static final String DISCOGS_DATA_URL = "https://data.discogs.com/";
-  private static final String DIRECT_BUCKET_URL =
-      "https://discogs-data-dumps.s3.us-west-2.amazonaws.com/";
   private static final String LEGACY_BUCKET_URL =
       "https://discogs-data.s3-us-west-2.amazonaws.com";
   private static final String USER_AGENT =
@@ -289,9 +287,6 @@ public class DefaultDumpSupplier implements DumpSupplier {
   }
 
   private URL createDownloadUrl(String uri) throws MalformedURLException {
-    if (DISCOGS_DATA_URL.equals(discogsDataUrl)) {
-      return URI.create(DIRECT_BUCKET_URL + uri).toURL();
-    }
     return URI.create(
             discogsDataUrl + "?download=" + URLEncoder.encode(uri, StandardCharsets.UTF_8))
         .toURL();
--- src/test/java/io/dsub/discogs/batch/dump/DefaultDumpSupplierUnitTest.java
+++ src/test/java/io/dsub/discogs/batch/dump/DefaultDumpSupplierUnitTest.java
@@ -197,8 +197,7 @@ class DefaultDumpSupplierUnitTest {
               assertThat(dump.getSize()).isEqualTo(-1L);
               assertThat(dump.getETag()).isEqualTo(dump.getUriString());
               assertThat(dump.getUrl().toString())
-                  .startsWith(
-                      "https://discogs-data-dumps.s3.us-west-2.amazonaws.com/data/2026/");
+                  .startsWith("https://data.discogs.com/?download=data%2F2026%2F");
               assertThat(dump.getChecksumUrl().toString()).isEqualTo(julyManifestUrl);
             });
     verify(dumpSupplier, times(1)).getDiscogsManifestSource(augustManifestUrl);
@@ -525,9 +524,9 @@ class DefaultDumpSupplierUnitTest {
   }
 
   private String manifestUrl(String dateStamp) {
-    return "https://discogs-data-dumps.s3.us-west-2.amazonaws.com/data/"
+    return "https://data.discogs.com/?download=data%2F"
         + dateStamp.substring(0, 4)
-        + "/discogs_"
+        + "%2Fdiscogs_"
         + dateStamp
         + "_CHECKSUM.txt";
   }
